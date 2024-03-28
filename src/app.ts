58fd582b5a401a31b8def3f2a333882ca37bce6b
Bernardo <bernardobbm334@gmail.com>
2024-03-28 20:21:09 -0300

    feat: função global para lidar com erros desconhecidos

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,22 @@
 import fastify from 'fastify'
+import { ZodError } from 'zod'
+import { env } from './env'
 import { usersRoutes } from './http/controllers/users/routes'
 
 export const app = fastify()
 
 app.register(usersRoutes)
+
+app.setErrorHandler((error, _, reply) => {
+  if (error instanceof ZodError) {
+    return reply
+      .status(400)
+      .send({ message: 'Validation error.', issues: error.format() })
+  }
+
+  if (env.NODE_ENV !== 'production') {
+    console.log(error)
+  }
+
+  return reply.status(500).send('Internal server error.')
+})
