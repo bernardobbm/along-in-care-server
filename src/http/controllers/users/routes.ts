b214d87282447db0bf7cc489ce16952e5a02d600
Bernardo <bernardobbm334@gmail.com>
2024-03-28 19:08:41 -0300

    feat: controller da rota de cadastro separado em arquivo específico

--- src/http/controllers/users/routes.ts
+++ src/http/controllers/users/routes.ts
@@ -1,29 +1,6 @@
 import { FastifyInstance } from 'fastify'
-import { z } from 'zod'
-import { prisma } from '../../../lib/prisma'
+import { register } from './register'
 
 export async function usersRoutes(app: FastifyInstance) {
-  app.post('/users', async (request, reply) => {
-    const registerBodySchema = z.object({
-      name: z.string(),
-      lastName: z.string(),
-      email: z.string().email(),
-      password: z.string().min(6),
-    })
-
-    const { name, lastName, email, password } = registerBodySchema.parse(
-      request.body,
-    )
-
-    await prisma.caregiver.create({
-      data: {
-        name,
-        last_name: lastName,
-        email,
-        password_hash: password,
-      },
-    })
-
-    return reply.status(201).send()
-  })
+  app.post('/users', register)
 }
