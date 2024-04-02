87dcba09a6d3cca509af2e15d2dcf023e69db487
Bernardo <bernardobbm334@gmail.com>
2024-04-02 00:21:27 -0300

    feat: adição do controller e rota de autenticação

--- src/http/controllers/users/routes.ts
+++ src/http/controllers/users/routes.ts
@@ -1,6 +1,8 @@
 import { FastifyInstance } from 'fastify'
+import { authenticate } from './authenticate'
 import { register } from './register'
 
 export async function usersRoutes(app: FastifyInstance) {
   app.post('/users', register)
+  app.post('/sessions', authenticate)
 }
