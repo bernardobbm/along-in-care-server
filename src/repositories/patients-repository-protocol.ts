6f9db22f055379f02478440fee735199ba7be62e
Bernardo <bernardobbm334@gmail.com>
2024-04-23 13:12:57 -0300

    feat: adição de interface para os repositories da tabela de pacientes

---
+++ src/repositories/patients-repository-protocol.ts
@@ -0,0 +1,9 @@
+import { Patient, Prisma } from '@prisma/client'
+
+export interface PatientsRepositoryProtocol {
+  create(
+    data: Prisma.PatientUncheckedCreateInput,
+    caregiverId: string,
+  ): Promise<Patient>
+  findByCpf(cpf: string): Promise<Patient | null>
+}
