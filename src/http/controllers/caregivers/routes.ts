ee0fadece3f4ae1fdadf18f216eda91a328e0b0c
Bernardo <bernardobbm334@gmail.com>
2024-04-23 13:51:59 -0300

    feat: adição das rotas criadas para cuidadores ao arquivo de rotas específico

--- src/http/controllers/caregivers/routes.ts
+++ src/http/controllers/caregivers/routes.ts
@@ -1,11 +1,17 @@
 import { FastifyInstance } from 'fastify'
+
+import { verifyJWT } from '../../middlewares/verify-jwt'
 import { authenticate } from './authenticate'
+import { profile } from './profile'
+import { refresh } from './refresh'
 import { register } from './register'
 
 export async function caregiversRoutes(app: FastifyInstance) {
   app.post('/caregivers', register)
   app.post('/sessions', authenticate)
 
+  app.patch('/token/refresh', refresh)
+
   /** Authenticated routes */
-  // app.get('/me', middleware for jwt, profile)
+  app.get('/me', { onRequest: [verifyJWT] }, profile)
 }
