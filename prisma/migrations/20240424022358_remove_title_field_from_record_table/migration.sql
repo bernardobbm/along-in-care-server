10b2cbae100cd27f646502b8b6e89696e38f6892
Bernardo <bernardobbm334@gmail.com>
2024-04-23 23:26:17 -0300

    refactor: campo de titulo removido da tabela de registros

---
+++ prisma/migrations/20240424022358_remove_title_field_from_record_table/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `title` on the `records` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "records" DROP COLUMN "title";
