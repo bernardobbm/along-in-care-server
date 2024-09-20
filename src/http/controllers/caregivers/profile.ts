b87bd54f8c9bc2523f09ff45efe883d4eaad5954
Bernardo <bernardobbm334@gmail.com>
2024-09-19 23:05:02 -0300

    feat: Adição de bloco try catch para melhor tratativa em caso de erros

--- src/http/controllers/caregivers/profile.ts
+++ src/http/controllers/caregivers/profile.ts
@@ -1,17 +1,26 @@
 import { FastifyReply, FastifyRequest } from 'fastify'
+import { ResourceNotFoundError } from '../../../use-cases/errors/resource-not-found-error'
 import { makeGetCaregiverProfileUseCase } from '../../../use-cases/factories/make-get-caregiver-profile-use-case'
 
 export async function profile(request: FastifyRequest, reply: FastifyReply) {
   const getUserProfile = makeGetCaregiverProfileUseCase()
 
-  const { caregiver } = await getUserProfile.execute({
-    caregiverId: request.user.sub,
-  })
+  try {
+    const { caregiver } = await getUserProfile.execute({
+      caregiverId: request.user.sub,
+    })
 
-  return reply.status(200).send({
-    caregiver: {
-      ...caregiver,
-      password_hash: undefined,
-    },
-  })
+    reply.status(200).send({
+      caregiver: {
+        ...caregiver,
+        password_hash: undefined,
+      },
+    })
+  } catch (err) {
+    if (err instanceof ResourceNotFoundError) {
+      reply.status(404).send({ message: err.message })
+    }
+
+    throw err
+  }
 }
