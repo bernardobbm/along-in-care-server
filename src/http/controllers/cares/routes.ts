29b6350d7da7a42ee40a8e0a156f249f26c7ec4d
Bernardo <bernardobbm334@gmail.com>
2024-09-19 23:11:31 -0300

    feat: Adição de nova rota e middlewares para verificação do papel do usuário

--- src/http/controllers/cares/routes.ts
+++ src/http/controllers/cares/routes.ts
@@ -1,16 +1,35 @@
 import { FastifyInstance } from 'fastify'
 
 import { verifyJWT } from '../../middlewares/verify-jwt'
+import { verifyUserRole } from '../../middlewares/verify-user-role'
 import { create } from './create'
 import { createAlimentation } from './create-alimentation'
 import { createHygiene } from './create-hygiene'
 import { createMedication } from './create-medication'
+import { fetchAll } from './fetch-all'
 
 export async function caresRoutes(app: FastifyInstance) {
   app.addHook('onRequest', verifyJWT)
 
-  app.post('/cares/others', create)
-  app.post('/cares/hygiene', createHygiene)
-  app.post('/cares/medication', createMedication)
-  app.post('/cares/alimentation', createAlimentation)
+  app.get('/cares', fetchAll)
+
+  app.post('/cares/others', { onRequest: verifyUserRole('PRIMARY') }, create)
+
+  app.post(
+    '/cares/hygiene',
+    { onRequest: verifyUserRole('PRIMARY') },
+    createHygiene,
+  )
+
+  app.post(
+    '/cares/medication',
+    { onRequest: verifyUserRole('PRIMARY') },
+    createMedication,
+  )
+
+  app.post(
+    '/cares/alimentation',
+    { onRequest: verifyUserRole('PRIMARY') },
+    createAlimentation,
+  )
 }
