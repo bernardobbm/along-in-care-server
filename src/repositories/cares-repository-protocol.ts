00500cb890446b9fdea0efe02a52d0dd9326428d
Bernardo <bernardobbm334@gmail.com>
2024-09-23 20:49:02 -0300

    feat: adição de novos parâmetros e alteração nas interfaces

--- src/repositories/cares-repository-protocol.ts
+++ src/repositories/cares-repository-protocol.ts
@@ -1,16 +1,18 @@
 import { Care, Prisma } from '@prisma/client'
 
-export type Cares = {
+export type SpecificCares = {
   medication?: Prisma.MedicationCreateWithoutCareInput | null
   hygiene?: Prisma.HygieneCreateWithoutCareInput | null
   alimentation?: Prisma.AlimentationCreateWithoutCareInput | null
 }
 
+export interface Cares extends Care, SpecificCares {}
 export interface CaresRepositoryProtocol {
   create(
     patientId: string,
+    careDays: number[],
     data: Prisma.CareCreateManyInput,
-    optionalCareFields?: Cares,
+    optionalCareFields?: SpecificCares,
   ): Promise<Care>
   findMany(patientId: string): Promise<Cares[]>
 }
