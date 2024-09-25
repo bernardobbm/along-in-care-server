8c90c26319cd41f31cf75f4d4fe127cf388b3c26
Bernardo <bernardobbm334@gmail.com>
2024-09-24 21:43:06 -0300

    refactor: Controller para acesso aos cuidados modificada para receber o id de paciente do front-end

--- src/http/controllers/cares/fetch-all.ts
+++ src/http/controllers/cares/fetch-all.ts
@@ -1,20 +1,19 @@
 import { FastifyReply, FastifyRequest } from 'fastify'
+import { z } from 'zod'
 import { makeFetchCaresUseCase } from '../../../use-cases/factories/make-fetch-cares-use-case'
 
 export async function fetchAll(request: FastifyRequest, reply: FastifyReply) {
+  console.log(request.body)
+
+  const fetchAllBodySchema = z.object({
+    patientId: z.string(),
+  })
+
+  const { patientId } = fetchAllBodySchema.parse(request.body)
+
   const fetchAllCares = makeFetchCaresUseCase()
 
-  const { patientId } = request.cookies
+  const { cares } = await fetchAllCares.execute({ patientId })
 
-  console.log(patientId)
-
-  if (patientId) {
-    const { cares } = await fetchAllCares.execute({ patientId })
-
-    reply.code(200).send(cares)
-  }
-
-  reply
-    .code(404)
-    .send({ message: 'Não foi possível acessar os dados solicitados' })
+  reply.code(200).send(cares)
 }
