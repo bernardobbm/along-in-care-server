4d33cb5cc001695ef9915758ad3c06284d08a8d0
Bernardo <bernardobbm334@gmail.com>
2024-10-12 19:18:38 -0300

    feat: adição das funções responsáveis para administração dos registros a interface dos repositórios de registro

--- src/repositories/records-repository-protocol.ts
+++ src/repositories/records-repository-protocol.ts
@@ -1,5 +1,12 @@
 import { Prisma, Record } from '@prisma/client'
 
 export interface RecordsRepositoryProtocol {
-  create(data: Prisma.RecordUncheckedCreateInput): Promise<Record>
+  create(data: Prisma.RecordCreateInput, careId: string): Promise<Record>
+  remove(recordId: string): Promise<void>
+  findById(recordId: string): Promise<Record | null>
+  findManyByCare(careId: string): Promise<Record[]>
+  findRecordsWithinLastHour(
+    careId: string,
+    registerHour: Date | string,
+  ): Promise<Record | Record[] | null>
 }
