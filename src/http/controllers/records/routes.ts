f4087c0a2f94b4ec1806aac330dd010289be4353
Bernardo <bernardobbm334@gmail.com>
2024-10-12 18:49:57 -0300

    feat: adição das rotas responsáveis pelos registros e seus arquivos controller

--- src/http/controllers/records/routes.ts
+++ src/http/controllers/records/routes.ts
@@ -2,9 +2,15 @@ import { FastifyInstance } from 'fastify'
 
 import { verifyJWT } from '../../middlewares/verify-jwt'
 import { create } from './create'
+import { getManyByCare } from './get-many-by-care'
+import { getRecord } from './get-record'
+import { remove } from './remove'
 
 export async function recordsRoutes(app: FastifyInstance) {
   app.addHook('onRequest', verifyJWT)
 
-  app.post('/records', create)
+  app.post('/records/:careId', create)
+  app.get('/records/:recordId', getRecord)
+  app.get('/records/:careId/all', getManyByCare)
+  app.delete('/records/:recordId', remove)
 }
