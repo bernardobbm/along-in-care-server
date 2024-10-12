301d34fd1949931889c1463fe4cd44ff9943c4ba
Bernardo <bernardobbm334@gmail.com>
2024-10-12 19:24:25 -0300

    feat: implementação das funções adicionadas a interface de repositório dos registros no repositório em memória

--- src/repositories/in-memory/in-memory-records-repository.ts
+++ src/repositories/in-memory/in-memory-records-repository.ts
@@ -5,17 +5,57 @@ import { RecordsRepositoryProtocol } from '../records-repository-protocol'
 export class InMemoryRecordsRepository implements RecordsRepositoryProtocol {
   public items: Record[] = []
 
-  async create(data: Prisma.RecordUncheckedCreateInput) {
+  async create(data: Prisma.RecordCreateInput, careId: string) {
     const record = {
       id: randomUUID(),
       was_accomplished: data.was_accomplished,
       time_of_accomplishment: new Date(data.time_of_accomplishment),
       description: data.description,
-      care_id: data.care_id,
+      care_id: careId,
     }
 
     this.items.push(record)
 
     return record
+  }
+
+  async findById(recordId: string) {
+    const record = this.items.find((record) => record.id === recordId)
+
+    if (!record) return null
+
+    return record
+  }
+
+  async findManyByCare(careId: string) {
+    const records = this.items.filter((record) => record.care_id === careId)
+
+    return records
+  }
+
+  async findRecordsWithinLastHour(careId: string, registerHour: Date | string) {
+    const oneHourBeforeFromGivenDate = new Date(
+      new Date(registerHour).getTime() - 60 * 60 * 1000,
+    )
+
+    const records = this.items.filter((record) => record.care_id === careId)
+
+    const filteredRecords = records.filter((record) => {
+      if (oneHourBeforeFromGivenDate > record.time_of_accomplishment) {
+        return false
+      }
+
+      return record
+    })
+
+    if (filteredRecords.length === 0) return null
+
+    return filteredRecords
+  }
+
+  async remove(recordId: string) {
+    this.items = this.items.filter((item) => {
+      return item.id !== recordId
+    })
   }
 }
