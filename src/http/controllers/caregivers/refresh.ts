be0666cd056b80ab407371a6022d029961462a85
Bernardo <bernardobbm334@gmail.com>
2024-12-08 19:29:48 -0300

    feat: adição de bloco try/catch em volta das requisições

--- src/http/controllers/caregivers/refresh.ts
+++ src/http/controllers/caregivers/refresh.ts
@@ -1,36 +1,40 @@
 import { FastifyReply, FastifyRequest } from 'fastify'
 
 export async function refresh(request: FastifyRequest, reply: FastifyReply) {
-  await request.jwtVerify({ onlyCookie: true })
+  try {
+    await request.jwtVerify()
 
-  const { role } = request.user
+    const { role } = request.user
 
-  const token = await reply.jwtSign(
-    { role },
-    {
-      sign: {
-        sub: request.user.sub,
+    const token = await reply.jwtSign(
+      { role },
+      {
+        sign: {
+          sub: request.user.sub,
+        },
       },
-    },
-  )
+    )
 
-  const refreshToken = await reply.jwtSign(
-    { role },
-    {
-      sign: {
-        sub: request.user.sub,
-        expiresIn: '7d',
+    const refreshToken = await reply.jwtSign(
+      { role },
+      {
+        sign: {
+          sub: request.user.sub,
+          expiresIn: '7d',
+        },
       },
-    },
-  )
+    )
 
-  return reply
-    .setCookie('refreshToken', refreshToken, {
-      path: '/',
-      secure: true,
-      sameSite: true,
-      httpOnly: true,
-    })
-    .status(200)
-    .send({ token })
+    return reply
+      .setCookie('refreshToken', refreshToken, {
+        path: '/',
+        secure: true,
+        sameSite: true,
+        httpOnly: true,
+      })
+      .status(200)
+      .send({ token, refreshToken })
+  } catch (err) {
+    return reply.code(401).send({ message: 'Token inválido.' })
+  }
 }
