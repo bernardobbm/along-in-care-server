2f5926e13d66c2f4659cdcb48c839e0dc338bf6f
Bernardo <bernardobbm334@gmail.com>
2024-12-20 02:37:15 -0300

    fix: remoção de verificação no papel do cuidador para criação de cuidados

--- src/http/controllers/cares/routes.ts
+++ src/http/controllers/cares/routes.ts
@@ -1,7 +1,5 @@
 import { FastifyInstance } from 'fastify'
 
-import { verifyJWT } from '../../middlewares/verify-jwt'
-import { verifyUserRole } from '../../middlewares/verify-user-role'
 import { create } from './create'
 import { fetchAll } from './fetch-all'
 import { getCare } from './get-care'
@@ -9,13 +7,11 @@ import { remove } from './remove'
 import { updateCare } from './update'
 
 export async function caresRoutes(app: FastifyInstance) {
-  app.addHook('onRequest', verifyJWT)
-
   app.get('/cares/:patientId', fetchAll)
 
   app.get('/cares/info/:careId', getCare)
 
-  app.post('/cares/create', { onRequest: verifyUserRole('PRIMARY') }, create)
+  app.post('/cares/create', create)
 
   app.delete('/cares/:careId', remove)
 
